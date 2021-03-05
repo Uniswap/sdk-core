a3452c816a3784b09392d01a2a4b7d4f5e634df4
Moody Salem <moody.salem@gmail.com>
2021-03-05 14:01:37 -0600

    fix sqrt function

--- src/utils/babylonianSqrt.ts
+++ src/utils/babylonianSqrt.ts
@@ -5,14 +5,14 @@ import { ONE, THREE, TWO, ZERO } from '../constants'
 export default function babylonianSqrt(y: JSBI): JSBI {
   let z: JSBI = ZERO
   let x: JSBI
-  if (y > THREE) {
+  if (JSBI.greaterThan(y, THREE)) {
     z = y
     x = JSBI.add(JSBI.divide(y, TWO), ONE)
-    while (x < z) {
+    while (JSBI.lessThan(x, z)) {
       z = x
       x = JSBI.divide(JSBI.add(JSBI.divide(y, x), x), TWO)
     }
-  } else if (y !== ZERO) {
+  } else if (JSBI.notEqual(y, ZERO)) {
     z = ONE
   }
   return z
