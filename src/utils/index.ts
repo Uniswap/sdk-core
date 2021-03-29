5a741a3b04468acfc732deab3a228f592a331aed
Moody Salem <moody.salem@gmail.com>
2021-03-29 09:05:26 -0500

    add sqrt function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import sortedInsert from './sortedInsert'
 import validateAndParseAddress from './validateAndParseAddress'
+import sqrt from './sqrt'
 
-export { sortedInsert, validateAndParseAddress }
+export { sortedInsert, validateAndParseAddress, sqrt }
