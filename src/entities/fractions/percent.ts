db01d55d4d3f37163ae4e0396f3fffe74047b089
Moody Salem <moody.salem@gmail.com>
2021-05-09 11:20:20 -0500

    always return a percent from percent math operations

--- src/entities/fractions/percent.ts
+++ src/entities/fractions/percent.ts
@@ -1,10 +1,35 @@
 import JSBI from 'jsbi'
-import { Rounding } from '../../constants'
+import { BigintIsh, Rounding } from '../../constants'
 import Fraction from './fraction'
 
 const _100_PERCENT = new Fraction(JSBI.BigInt(100))
 
+function toPercent(fraction: Fraction): Percent {
+  return new Percent(fraction.numerator, fraction.denominator)
+}
+
 export default class Percent extends Fraction {
+  /**
+   * This boolean prevents a fraction from being interpreted as a Percent
+   */
+  public readonly isPercent: boolean = true
+
+  add(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.add(other))
+  }
+
+  subtract(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.subtract(other))
+  }
+
+  multiply(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.multiply(other))
+  }
+
+  divide(other: Fraction | BigintIsh): Percent {
+    return toPercent(super.divide(other))
+  }
+
   public toSignificant(significantDigits: number = 5, format?: object, rounding?: Rounding): string {
     return this.multiply(_100_PERCENT).toSignificant(significantDigits, format, rounding)
   }
