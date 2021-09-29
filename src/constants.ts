aea5c8f0b45d452cdade90734b3ea3d78bf9875c
Osa <nichita.osadciuc@latoken.com>
2021-09-30 00:54:52 +0300

    add LA

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  LA = 41
 }
 
 export enum TradeType {
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -67,5 +67,6 @@ export const WETH9: { [chainId in ChainId]: Token } = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH9', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH9', 'Wrapped Ether'),
+  [ChainId.LA]: new Token(41, '0x9861f08a6bA30CBdA167E9F62ba29C002514C561', 18, 'WETH9', 'WETH')
 }
