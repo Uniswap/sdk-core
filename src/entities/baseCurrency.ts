223dd39d41f3d9530a8cdf76f386f68c984bebdc
Sujan Adhikari <71167057+asujan205@users.noreply.github.com>
2023-07-19 23:16:56 +0545

    Update baseCurrency.ts
    #BaseCurrencyprobleam

--- src/entities/baseCurrency.ts
+++ src/entities/baseCurrency.ts
@@ -53,7 +53,7 @@ export abstract class BaseCurrency {
    * Returns whether this currency is functionally equivalent to the other currency
    * @param other the other currency
    */
-  public abstract equals(other: Currency): boolean
+  public abstract equals(other: BaseCurrency): boolean
 
   /**
    * Return the wrapped version of this currency that can be used with the Uniswap contracts. Currencies must
