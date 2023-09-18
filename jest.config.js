88013414941f721e77267f84493b296b11658e40
Koray Koska <11356621+koraykoska@users.noreply.github.com>
2023-09-18 17:43:06 +0100

    fix: remove comments

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  // preset: 'ts-jest',
-  // testEnvironment: 'node',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
