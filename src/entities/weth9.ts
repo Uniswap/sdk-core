07c6fffb2650f5e41445426343ac9c650e6c2873
Alice <34962750+hensha256@users.noreply.github.com>
2023-10-16 16:23:26 +0100

    Wrapped avax support (#92)

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -20,5 +20,5 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [56]: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
   [137]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 18, 'WMATIC', 'Wrapped MATIC'),
-  [43114]: new Token(43114, '0x4200000000000000000000000000000000000006', 18, 'WAVAX', 'Wrapped AVAX')
+  [43114]: new Token(43114, '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', 18, 'WAVAX', 'Wrapped AVAX')
 }
