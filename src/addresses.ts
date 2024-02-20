296787c26744f2b0af8280fd891755cb04eb78fa
Dallin Larsen <dallin.j.larsen@gmail.com>
2024-02-20 15:04:57 -0700

    Updated code to correctly access addresses

--- src/addresses.ts
+++ src/addresses.ts
@@ -310,7 +310,7 @@ export const MIXED_ROUTE_QUOTER_V1_ADDRESSES: AddressMap = SUPPORTED_CHAINS.redu
 }, {})
 
 export const SWAP_ROUTER_02_ADDRESSES = (chainId: number) => {
-  if (chainId in SUPPORTED_CHAINS) {
+  if (SUPPORTED_CHAINS.includes(chainId)) {
     const id = chainId as SupportedChainsType
     return CHAIN_TO_ADDRESSES_MAP[id].swapRouter02Address ?? '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
   }
